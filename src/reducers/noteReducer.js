9b97bbc60f6c6c10d884bc604ad0f70ef0f60f17
JaviMorin <javiermc_23@hotmail.es>
2023-02-08 00:05:53 +0100

    Comunicarse con el servidor en la aplicación redux.

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -1,28 +1,17 @@
-const initialState = [
-  {
-    content: 'reducer defines how redux store works',
-    important: true,
-    id: 1,
-  },
-  {
-    content: 'state of store can contain any data',
-    important: false,
-    id: 2,
-  },
-]
-
-const noteReducer = (state = initialState, action) => {
+const noteReducer = (state = [], action) => {
   console.log('ACTION: ', action)
   switch(action.type) {
     case 'NEW_NOTE':
         return [...state, action.data]
+    case 'INIT_NOTES':
+      return action.data
     case 'TOGGLE_IMPORTANCE': {
       const id = action.data.id
       const noteToChange = state.find(n => n.id === id)
       const changedNote = { 
         ...noteToChange, 
         important: !noteToChange.important 
-      }
+      } 
       return state.map(note =>
         note.id !== id ? note : changedNote 
       )
@@ -32,24 +21,24 @@ const noteReducer = (state = initialState, action) => {
   }
 }
 
-const generateId = () =>
-  Number((Math.random() * 1000000).toFixed(0))
-
-export const createNote = (content) => {
-  return {
-    type: 'NEW_NOTE',
-    data: {
-      content,
-      important: false,
-      id: generateId()
+export const createNote = (data) => {
+    return {
+      type: 'NEW_NOTE',
+      data,
     }
   }
-}
 
 export const toggleImportanceOf = (id) => {
   return {
     type: 'TOGGLE_IMPORTANCE',
     data: { id }
+  }
+}
+
+export const initializeNotes = (notes) => {
+  return {
+    type: 'INIT_NOTES',
+    data: notes,
   }
 }
 
